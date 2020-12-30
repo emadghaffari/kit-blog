6a54f05e02e944020435f25f7b922393b48822a4
Emad <emadghaffariii@gmail.com>
2020-12-30 17:49:40 +0000

    add mongodb

--- notificator/pkg/service/service.go
+++ notificator/pkg/service/service.go
@@ -3,9 +3,15 @@ package service
 import (
 	"context"
 	"log"
+	"time"
 
-	"github.com/gofrs/uuid"
 	"github.com/opentracing/opentracing-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/emadghaffari/api-teacher/utils/random"
 )
 
 // NotificatorService describes the service.
@@ -14,12 +20,26 @@ type NotificatorService interface {
 	Send(ctx context.Context, to, body string) (id string, err error)
 }
 
-type basicNotificatorService struct{}
+type basicNotificatorService struct {
+	db *mongo.Collection
+}
 
 func (b *basicNotificatorService) Send(ctx context.Context, to string, body string) (id string, err error) {
-	u, err := uuid.NewV4()
+	values := bson.M{
+		"to":    to,
+		"body":  body,
+		"notif": random.Rand(10000, 999999),
+	}
+	res, err := b.db.InsertOne(context.Background(), values)
+
 	if err != nil {
-		log.Printf("failed to generate UUID: %v", err)
+		log.Printf("Error in insert data to mongodb: %v", err)
+		return "Fail", err
+	}
+
+	if _, ok := res.InsertedID.(primitive.ObjectID); !ok {
+		log.Printf("Error in get oid from res")
+		return "Fail", err
 	}
 
 	if parent := opentracing.SpanFromContext(ctx); parent != nil {
@@ -28,16 +48,23 @@ func (b *basicNotificatorService) Send(ctx context.Context, to string, body stri
 			span := tracer.StartSpan("notification", opentracing.ChildOf(pctx))
 			defer span.Finish()
 
-			span.SetTag("id NOTIF", u.String())
+			span.SetTag("sended notification to", to)
 		}
 	}
 
-	return u.String(), err
+	return "SUCCESS", err
 }
 
 // NewBasicNotificatorService returns a naive, stateless implementation of NotificatorService.
 func NewBasicNotificatorService() NotificatorService {
-	return &basicNotificatorService{}
+
+	col, err := initMongoDB()
+	if err != nil {
+		return new(basicNotificatorService)
+	}
+	return &basicNotificatorService{
+		db: col,
+	}
 }
 
 // New returns a NotificatorService with all of the expected middleware wired in.
@@ -47,4 +74,24 @@ func New(middleware []Middleware) NotificatorService {
 		svc = m(svc)
 	}
 	return svc
+}
+
+func initMongoDB() (*mongo.Collection, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:example@mongo:27017"))
+	if err != nil {
+		log.Printf(err.Error())
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Printf(err.Error())
+		return nil, err
+	}
+
+	users := client.Database("kit-notification").Collection("notification")
+
+	return users, nil
+
 }
