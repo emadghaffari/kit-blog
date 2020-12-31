6c1de791868e38b5323a862c5cb31a8831cffdc9
Emad <emadghaffariii@gmail.com>
2020-12-31 12:38:35 +0000

    [config] implement basic config for services

---
+++ comments/config/main.go
@@ -0,0 +1,41 @@
+package config
+
+import "github.com/hashicorp/vault/api"
+
+// Confs var
+var Confs configs
+
+type (
+	// configs struct
+	configs struct {
+		Comments struct {
+			Host       string
+			Path       string
+			DebugAddr  string
+			HTTPAddr   string
+			GrpcAddr   string
+			ThriftAddr string
+		}
+		Posts struct {
+			Host       string
+			Path       string
+			DebugAddr  string
+			HTTPAddr   string
+			GrpcAddr   string
+			ThriftAddr string
+		}
+		Users struct {
+			Host       string
+			Path       string
+			DebugAddr  string
+			HTTPAddr   string
+			GrpcAddr   string
+			ThriftAddr string
+		}
+		Vault struct {
+			Address string
+			Token   string
+			Logical *api.Logical
+		}
+	}
+)
